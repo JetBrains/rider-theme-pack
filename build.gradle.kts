fb61da7249318697aecb11d79a6df29a85dc8871
Mikhail Filippov <mikhail@filippov.me>
2020-02-28 12:21:31 +0300

    Prepare to publish in plugin gallery.

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,19 +1,20 @@
 plugins {
     id("org.jetbrains.intellij") version "0.4.13"
+    id("me.filippov.gradle.jvm.wrapper") version "0.9.3"
 }
 
 repositories {
     mavenCentral()
 }
 
+version = "0.1"
+
 intellij {
-    val BuildForIDEA = true
-    if (BuildForIDEA) {
-        version = "2019.3"
-    } else {
+    val useRiderSdk = System.getProperty("useRiderSdk")?.toBoolean() ?: false
+    if (useRiderSdk) {
         type = "RD"
-        version = "2019.3"
     }
+    version = "2019.3"
     pluginName = "Rider UI Theme Pack"
 
     tasks {
@@ -23,10 +24,12 @@ intellij {
 
         // Initially introduced in:
         // https://github.com/JetBrains/ForTea/blob/master/Frontend/build.gradle.kts
-        withType<org.jetbrains.intellij.tasks.RunIdeTask> {
-            // IDEs from SDK are launched with 512m by default, which is not enough for Rider.
-            // Rider uses this value when launched not from SDK.
-            maxHeapSize = "1500m"
+        if (!useRiderSdk) {
+            withType<org.jetbrains.intellij.tasks.RunIdeTask> {
+                // IDEs from SDK are launched with 512m by default, which is not enough for Rider.
+                // Rider uses this value when launched not from SDK.
+                maxHeapSize = "1500m"
+            }
         }
         withType<org.jetbrains.intellij.tasks.PatchPluginXmlTask> {
             updateSinceUntilBuild = true
