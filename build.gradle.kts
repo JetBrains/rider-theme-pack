194b1dcb41b0dbc8ac3bbe94bf603221991bc8d5
Konstantin Homatyano <konstantin.homatyano@jetbrains.com>
2023-09-28 14:38:37 +0200

    Adjusted the plugin's settings: compatible with Rider 232.*

--- build.gradle.kts
+++ build.gradle.kts
@@ -27,7 +27,6 @@ intellij {
         buildSearchableOptions {
             enabled = false
         }
-
         // Initially introduced in:
         // https://github.com/JetBrains/ForTea/blob/master/Frontend/build.gradle.kts
         if (!useRiderSdk) {
@@ -37,10 +36,10 @@ intellij {
                 maxHeapSize = "1500m"
             }
         }
-        withType<org.jetbrains.intellij.tasks.PatchPluginXmlTask> {
-            updateSinceUntilBuild.set(true)
-            sinceBuild.set("231.7665")
-            untilBuild.set("")
-        }
+//        withType<org.jetbrains.intellij.tasks.PatchPluginXmlTask> {
+//            updateSinceUntilBuild.set(true)
+//            sinceBuild.set("231.7665")
+//            untilBuild.set("")
+//        }
     }
 }
