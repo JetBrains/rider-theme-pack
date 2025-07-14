e16959e1968f8bb918c1859fc63adebf48d0f8c6
Ivan Migalev <ivan.migalev@jetbrains.com>
2025-07-14 18:17:34 +0200

    Build: bump the JVM wrapper, regenerate Gradle wrapper
    The main change is the JDK update to version 21 and Gradle to 8.14.3.

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,6 +1,6 @@
 plugins {
     id("org.jetbrains.intellij") version "1.15.0"
-    id("me.filippov.gradle.jvm.wrapper") version "0.14.0"
+    id("me.filippov.gradle.jvm.wrapper") version "0.15.0"
 }
 
 repositories {
